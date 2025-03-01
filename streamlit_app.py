d0e7157498bc84e9e76020bc1272ada35b174fd4
AStyler05 <alexander.rinecker@student.unisg.ch>
2025-03-01 16:18:21 +0000

    https://www.novasbe.unl.pt/Portals/0/Files/2024/Bachelor/Incoming_BSc_Factsheet_24_25.pdf?ver=2024-07-30-102708-013

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Alex first tries")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "That's just the beginning"
 )
+st.button("Click me for the beta version pro")
+st.radio("Pick one", ["standard", "pro"])
+st.number_input("Pick a number what you are willing to pay", 0, 10)               
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
